04fc4b868cf7ee4c07d4738a679e74133672306d
Mineng <mineng.top5@yandex.ru>
2022-02-08 19:20:58 +0500

    2 часть

--- main.py
+++ main.py
@@ -20,6 +20,36 @@ class Params:
         dic['spn'] = ','.join([str(self.spn), str(self.spn)])
         return dic
 
+    def increase(self):
+        if 0.001 <= self.spn <= 0.01:
+            self.spn -= 0.001
+        elif 0.01 <= self.spn <= 0.1:
+            self.spn -= 0.01
+        elif 0.1 <= self.spn <= 1:
+            self.spn -= 0.1
+        elif 1 <= self.spn <= 10:
+            self.spn -= 1
+        elif 10 <= self.spn <= 90:
+            self.spn -= 10
+
+    def decrease(self):
+        if 0.001 <= self.spn <= 0.01:
+            self.spn += 0.001
+        elif 0.01 <= self.spn <= 0.1:
+            self.spn += 0.01
+        elif 0.1 <= self.spn <= 1:
+            self.spn += 0.1
+        elif 1 <= self.spn <= 10:
+            self.spn += 1
+        elif 10 <= self.spn <= 80:
+            self.spn += 10
+
+    def update(self, key):
+        if key == pygame.K_PAGEUP:
+            self.increase()
+        elif key == pygame.K_PAGEDOWN:
+            self.decrease()
+
 
 def get_pick(dict_param):
     api_server = "http://static-maps.yandex.ru/1.x/"
@@ -33,21 +63,27 @@ def get_pick(dict_param):
         sys.exit()
 
 
-def draw_interface():
+def draw_interface(map):
     pygame.init()
     screen = pygame.display.set_mode((600, 450))
-    screen.blit(pygame.image.load(FILE_NAME), (0, 0))
-    pygame.display.flip()
-    while pygame.event.wait().type != pygame.QUIT:
-        pass
+    flag = True
+    while flag:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                flag = False
+            if event.type == pygame.KEYDOWN:
+                map.update(event.key)
+                get_pick(map.do_dict())
+        screen.blit(pygame.image.load(FILE_NAME), (0, 0))
+        pygame.display.flip()
     pygame.quit()
 
 
 longitude = 56.261068
 latitude = 58.007368
-spn = 0.003
+spn = 0.001
 l_params = 'map'
 
 map = Params(longitude, latitude, spn, l_params)
 get_pick(map.do_dict())
-draw_interface()
+draw_interface(map)
